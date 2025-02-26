b1202840f1a08685f5862bc0aac960d5751d8318
John <john.moreno@ibm.com>
2025-02-26 14:36:26 +0100

    add test url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://application-a1.1sdff4t8daw0.private.eu-de.codeengine.appdomain.cloud",
   },
 };
 
