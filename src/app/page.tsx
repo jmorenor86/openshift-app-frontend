36b3a71fbfd166499077235eba0674816c532444
John <91114836+jmorenor86@users.noreply.github.com>
2025-04-06 21:14:47 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MessageDisplay from "../components/MessageDisplay";
 export default function Home() {
   return (
     <div>
-      <h2>Backend Message:</h2>
+      <h2>Backend Message with trigger:</h2>
       <MessageDisplay />
     </div>
   );
