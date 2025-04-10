a0b1d8fe426487e1edb4be1b7ca9a5f3dece1ea8
John <91114836+jmorenor86@users.noreply.github.com>
2025-04-10 14:28:30 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MessageDisplay from "../components/MessageDisplay";
 export default function Home() {
   return (
     <div>
-      <h2>Backend pipeline openshift</h2>
+      <h2>Backend pipeline openshift:::</h2>
       <MessageDisplay />
     </div>
   );
